775fecb12f5e9bc08a07bbca780224cd97cfcc7a
SND\bovium_cp <SND\bovium_cp@81fbe566-5dc4-48c1-bdea-7421811ca204>
2008-11-07 20:07:15 +0000

    Added copyright and comments.
    git-svn-id: https://extensionoverflow.svn.codeplex.com/svn@16146 81fbe566-5dc4-48c1-bdea-7421811ca204

--- ExtensionMethods/ClassExtensions.cs
+++ ExtensionMethods/ClassExtensions.cs
@@ -1,12 +1,30 @@
-﻿using System;
+﻿//-------------------------------------------------------------------------------------------------
+//
+// ClassExtensions.cs - Extension Methods to be used on Classes.
+//
+// Copyright (c) 2008 C# .net Extension Method Collection Contributers
+//
+//-------------------------------------------------------------------------------------------------
+
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
 namespace ExtensionsMethods
 {
+	/// <summary>
+	/// Extension Methods to be used be Classes
+	/// </summary>
 	public static class ClassExtensions
 	{
+		/// <summary>
+		/// Throws an exception if the object called upon is null.
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="obj">The This object</param>
+		/// <param name="text">The text to be written on the exception</param>
 		public static void ThrowIfArgumentIsNull<T>(this T obj, string text) where T : class
 		{
 			if (obj == null) throw new ArgumentNullException(text + " not allowed to be null");
