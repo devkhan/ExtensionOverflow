ba3175a76ab9e2d6ffcbb26f189c82c7335a66f3
SND\bovium_cp <SND\bovium_cp@81fbe566-5dc4-48c1-bdea-7421811ca204>
2008-11-07 19:41:43 +0000

    git-svn-id: https://extensionoverflow.svn.codeplex.com/svn@16135 81fbe566-5dc4-48c1-bdea-7421811ca204

---
+++ ExtensionMethods/ClassExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ExtensionsMethods
+{
+	public static class ClassExtensions
+	{
+		public static void ThrowIfArgumentIsNull<T>(this T obj, string text) where T : class
+		{
+			if (obj == null) throw new ArgumentNullException(text + " not allowed to be null");
+		}
+	}
+}
