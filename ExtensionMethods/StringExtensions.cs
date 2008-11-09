2037d6395031ec224a12de721245164f546d9b0c
SND\chakrit_cp <SND\chakrit_cp@81fbe566-5dc4-48c1-bdea-7421811ca204>
2008-11-09 19:10:04 +0000

    - Added StringExtensions but have not yet figured out how to unit test many overloads. - Changed default namespace in ExtensionOverflow - Removed some auto-generated garbage
    git-svn-id: https://extensionoverflow.svn.codeplex.com/svn@16478 81fbe566-5dc4-48c1-bdea-7421811ca204

---
+++ ExtensionMethods/StringExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ExtensionMethod
+{
+    /// <summary>
+    /// <see cref="System.String" /> extensions.
+    /// </summary>
+    public static class StringExtensions
+    {
+
+        public static string Formats(this string s, object arg0)
+        {
+            return string.Format(s, arg0);
+        }
+        public static string Formats(this string s, object arg0, object arg1)
+        {
+            return string.Format(s, arg0, arg1);
+        }
+        public static string Formats(this string s, object arg0, object arg1, object arg2)
+        {
+            return string.Format(s, arg0, arg1, arg2);
+        }
+        public static string Formats(this string s, params object[] args)
+        {
+            return string.Format(s, args);
+        }
+
+        public static string Formats(this string s, IFormatProvider provider, params object[] args)
+        {
+            return string.Format(provider, s, args);
+        }
+
+
+    }
+}
