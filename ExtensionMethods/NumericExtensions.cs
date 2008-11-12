f81afaeed266a475aedf66bfb5ca9507f33ba41b
SND\BKristensen_cp <SND\BKristensen_cp@81fbe566-5dc4-48c1-bdea-7421811ca204>
2008-11-12 21:48:17 +0000

    git-svn-id: https://extensionoverflow.svn.codeplex.com/svn@17182 81fbe566-5dc4-48c1-bdea-7421811ca204

---
+++ ExtensionMethods/NumericExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ExtensionOverflow
+{
+    /// <summary>
+    /// Extensions that is general for all numeric types like int, decimal etc.
+    /// </summary>
+    class NumericExtensions
+    {
+    }
+}
