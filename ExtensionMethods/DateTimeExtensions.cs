65ad9dfb065d4f0b3ee17f87b97667773708bb26
SND\BKristensen_cp <SND\BKristensen_cp@81fbe566-5dc4-48c1-bdea-7421811ca204>
2008-11-14 08:07:58 +0000

    Added DateTime.Elapsed extension
    git-svn-id: https://extensionoverflow.svn.codeplex.com/svn@17344 81fbe566-5dc4-48c1-bdea-7421811ca204

---
+++ ExtensionMethods/DateTimeExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ExtensionOverflow
+{
+    /// <summary>
+    /// DateTime Extensions
+    /// </summary>
+    public static class DateTimeExtensions
+    {
+        #region Elapsed extension
+
+        /// <summary>
+        /// Percentages the of.
+        /// </summary>
+        /// <param name="datetime">The datetime.</param>
+        /// <returns></returns>
+        public static DateTime Elapsed(this DateTime datetime)
+        {
+            return DateTime.Now - datetime;
+        }
+
+        #endregion
+    }
+}
