07ed533801129bd4882bcc289b3d08031b201737
SND\BKristensen_cp <SND\BKristensen_cp@81fbe566-5dc4-48c1-bdea-7421811ca204>
2009-01-16 09:16:04 +0000

    Added IsValidUrl
    git-svn-id: https://extensionoverflow.svn.codeplex.com/svn@21889 81fbe566-5dc4-48c1-bdea-7421811ca204

--- ExtensionMethods/StringExtensions.cs
+++ ExtensionMethods/StringExtensions.cs
@@ -385,5 +385,13 @@ namespace ExtensionOverflow
             return result;
         }
         #endregion
+
+        #region IsValidUrl
+        public static bool IsValidUrl(this string text)
+        {
+            Regex rx = new Regex(@"http(s)?://([\w-]+\.)+[\w-]+(/[\w- ./?%&=]*)?");
+            return rx.IsMatch(text);
+        }
+        #endregion
     }
 }
