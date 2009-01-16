57270c289a71e5cd7ecf7d5c737c0cde7a186d28
SND\chakrit_cp <SND\chakrit_cp@81fbe566-5dc4-48c1-bdea-7421811ca204>
2009-01-16 15:39:26 +0000

    - Added ForEach for IEnumerable and basic unit tests.
    git-svn-id: https://extensionoverflow.svn.codeplex.com/svn@21910 81fbe566-5dc4-48c1-bdea-7421811ca204

--- ExtensionMethods/LinqExtensions.cs
+++ ExtensionMethods/LinqExtensions.cs
@@ -11,6 +11,8 @@ namespace ExtensionOverflow
     public static class LinqExtensions
     {
 
+        #region ToCSVString
+
         /// <summary>
         /// Converts the Linq data to a commaseperated string including header.
         /// </summary>
@@ -29,7 +31,7 @@ namespace ExtensionOverflow
         /// <returns></returns>
         public static string ToCSVString(this System.Linq.IOrderedQueryable data, string delimiter)
         {
-            return ToCSVString(data, "; ", null);
+            return ToCSVString(data, delimiter, null);
         }
 
         /// <summary>
@@ -45,8 +47,7 @@ namespace ExtensionOverflow
             string replaceFrom = delimiter.Trim();
             string replaceDelimiter = ";";
             System.Reflection.PropertyInfo[] headers = data.ElementType.GetProperties();
-            switch (replaceFrom)
-	        {
+            switch (replaceFrom) {
                 case ";":
                     replaceDelimiter = ":";
                     break;
@@ -56,43 +57,80 @@ namespace ExtensionOverflow
                 case "\t":
                     replaceDelimiter = "    ";
                     break;
-		        default:
+                default:
                     break;
-	        }
-            if (headers.Length > 0)
-            {
-                foreach (var head in headers)
-                {
+            }
+            if (headers.Length > 0) {
+                foreach (var head in headers) {
                     csvdata.Append(head.Name.Replace("_", " ") + delimiter);
                 }
                 csvdata.Append("\n");
             }
-            foreach (var row in data)
-            {
+            foreach (var row in data) {
                 var fields = row.GetType().GetProperties();
-                for (int i = 0; i < fields.Length; i++)
-			    {
+                for (int i = 0; i < fields.Length; i++) {
                     object value = null;
-                    try
-                    {
+                    try {
                         value = fields[i].GetValue(row, null);
-                    }
-                    catch { }
-                    if (value != null)
-                    {
+                    } catch { }
+                    if (value != null) {
                         csvdata.Append(value.ToString().Replace("\n", " \f").Replace("_", " ").Replace(replaceFrom, replaceDelimiter) + delimiter);
-                    }
-                    else
-                    {
+                    } else {
                         csvdata.Append(nullvalue);
                         csvdata.Append(delimiter);
                     }
-			    }
+                }
                 csvdata.Append("\n");
             }
             return csvdata.ToString();
         }
 
-        
+        #endregion
+
+        #region ForEach
+
+        /// <summary>
+        /// Runs an action against each of the element in the IEnumerable collection.
+        /// </summary>
+        /// <param name="enum">The source IEnumerable collection.</param>
+        /// <param name="act">The action to perform on each element.</param>
+        /// <returns>The IEnumerable collection itself, to allow chaining.</returns>
+        public static IEnumerable<T> ForEach<T>(this IEnumerable<T> @enum, Action<T> act)
+        {
+            @enum.ThrowIfArgumentIsNull("@enum");
+            act.ThrowIfArgumentIsNull("act");
+
+            // yield return may not be used here due to the fact that
+            // ForEach calls are expected to be run immediatly
+            foreach (var item in @enum) act(item);
+
+            return @enum;
+        }
+
+        /// <summary>
+        /// Runs a function against each of the element in the IEnumerable collection.
+        /// Discarding any return values.
+        /// </summary>
+        /// <param name="enum">The source IEnumerable collection.</param>
+        /// <param name="func">The function to run on each element.</param>
+        /// <returns>The IEnumerable collection itself, unmodified, to allow chaining.</returns>
+        /// <remarks>
+        /// This is unlike Select because the IEnumerable collection itself is left unmodified
+        /// and the return value is the same IEnumerable collection passed in.
+        /// </remarks>
+        public static IEnumerable<T> ForEach<T, _>(this IEnumerable<T> @enum, Func<T, _> func)
+        {
+            @enum.ThrowIfArgumentIsNull("@enum");
+            func.ThrowIfArgumentIsNull("func");
+
+            // yield return may not be used here due to the fact that
+            // ForEach calls are expected to be run immediatly
+            foreach (var item in @enum) func(item);
+
+            return @enum;
+        }
+
+        #endregion
+
     }
 }
